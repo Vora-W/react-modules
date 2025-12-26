6158be3d877300664ce8491398567e94b0440a84
Sirapop Chanphui <srp073536@gmail.com>
2025-12-26 14:51:20 +0700

    refactor: consolidate component structure into WebSections and remove individual component files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
-import ProductSection from "./components/ProductSection";
-import FAQ from "./components/FAQ";
+
+import {
+  Header,
+  Footer,
+  FAQ,
+  ProductSection,
+  MainContent,
+} from "./components/WebSections";
+
 
 function App() {
   return (
